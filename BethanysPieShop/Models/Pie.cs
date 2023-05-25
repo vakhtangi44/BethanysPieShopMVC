b0d081358bb58179279c6137dbd7111ee523595f
vakhtangi kapanadze <vakhtangi.kapanadze.4@outlook.com>
2023-05-25 13:08:25 +0400

    update

--- BethanysPieShop/Models/Pie.cs
+++ BethanysPieShop/Models/Pie.cs
@@ -1,7 +1,8 @@
 ﻿namespace BethanysPieShop.Models
 {
     public class Pie
-    {        public int PieId { get; set; }
+    {
+        public int PieId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? ShortDescription { get; set; }
         public string? LongDescription { get; set; }
